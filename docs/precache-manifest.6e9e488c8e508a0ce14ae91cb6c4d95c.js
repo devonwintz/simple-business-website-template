94bf9ebe6b399d8ff3751a5e9ea49db5692830bc
Devon Wintz <samuelwintz@gmail.com>
2020-09-10 22:39:55 -0400

    first commit

---
+++ docs/precache-manifest.6e9e488c8e508a0ce14ae91cb6c4d95c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddd24b7be7f23853ac669aa8e051a923",
+    "url": "/simple-business-website-template/index.html"
+  },
+  {
+    "revision": "1cc7b93b5998c13f6fb1",
+    "url": "/simple-business-website-template/static/css/main.d3440d34.chunk.css"
+  },
+  {
+    "revision": "0c740caa4177790b7a8d",
+    "url": "/simple-business-website-template/static/js/2.982d3db4.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/simple-business-website-template/static/js/2.982d3db4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cc7b93b5998c13f6fb1",
+    "url": "/simple-business-website-template/static/js/main.66858073.chunk.js"
+  },
+  {
+    "revision": "43a8f6cf3aa2b2c34ebe",
+    "url": "/simple-business-website-template/static/js/runtime-main.cf0bc627.js"
+  },
+  {
+    "revision": "efbac42603179e82f1e87f6dd0bad715",
+    "url": "/simple-business-website-template/static/media/ig-icon.efbac426.png"
+  },
+  {
+    "revision": "d7c6644ae020495730a8f1fee7924990",
+    "url": "/simple-business-website-template/static/media/twitter-icon.d7c6644a.png"
+  }
+]);
